5ada8e74a552f06847bb462c5a6b0b146bbfe6a3
Aaron  Jomy <75925957+maximusron@users.noreply.github.com>
2024-06-07 17:59:05 +0200

    Fix the classloading test

--- test/test_aclassloader.py
+++ test/test_aclassloader.py
@@ -8,15 +8,15 @@ test_dct = str(currpath.join("example01Dict"))
 def setup_module(mod):
     setup_make("example01")
 
-
 class TestACLASSLOADER:
 
     def setup_class(cls):
+        cls.test_dct = test_dct
         import cppyy
+        cls.example01 = cppyy.load_reflection_info(cls.test_dct)
 
-    @mark.xfail
     def test01_class_autoloading(self):
-        """Test whether a class can be found through .rootmap."""
+        """Test whether a class can be found"""
         import cppyy
         example01_class = cppyy.gbl.example01
         assert example01_class
